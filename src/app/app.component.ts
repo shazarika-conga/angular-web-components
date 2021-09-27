5df6115dca7b209c9c3dc78fbc086b20792caea7
Shabiq Hussain Hazarika <shazarika@conga.com>
2021-09-27 15:38:44 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'conga-component-library';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wc-angular';
+
+  handleClick(event: MouseEvent): void {
+    console.log(event);
+  }
 }
